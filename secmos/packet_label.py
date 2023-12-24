42dbef72ae7d9406d5a8a3395a6937abd73a690c
Yunchuan "Winslow" Hu <i@winsloweric.com>
2023-12-24 04:25:19 -0800

    patched multiple constraint violation

--- secmos/packet_label.py
+++ secmos/packet_label.py
@@ -11,22 +11,17 @@ def parse_service_line(line):
             return name, port, protocol
     return None, None, None
 
-def write_nftables_config(file, protocols):
-    # Write table header
-    file.write('table inet secmark_rule_demo {\n\n')
+def write_secmark_commands(file, name, port, protocol):
+    file.write(f'iptables -t mangle -A INPUT -p {protocol} --dport {port} -j SECMARK --selctx system_u:object_r:{name}_packet_t:s0\n')
+    file.write(f'iptables -t mangle -A OUTPUT -p {protocol} --dport {port} -j SECMARK --selctx system_u:object_r:{name}_packet_t:s0\n')
 
-    # Write secmark definitions and chain rules
-    for name, port in protocols:
-        file.write(f'    secmark {name}tag {{ "system_u:object_r:{name}_packet_t:s0" }}\n')
-    
-    file.write('\n    chain IN {\n')
-    file.write('        type filter hook input priority filter;\n\n')
+def write_attributes(file, protocols):
+    for name in protocols:
+        file.write(f'attribute {name}_packet_type;\n')
 
-    for name, port in protocols:
-        file.write(f'        tcp dport {port} meta secmark set "{name}tag"\n')
-
-    file.write('    }\n')
-    file.write('}\n')
+def write_types(file, protocols):
+    for name in protocols:
+        file.write(f'type {name}_packet_t, packet_type, {name}_packet_type;\n')
 
 def process_services_file():
     protocols = set()
@@ -35,11 +30,24 @@ def process_services_file():
         for line in services_file:
             if line.strip() and not line.startswith('#'):
                 name, port, protocol = parse_service_line(line)
-                if name and port and protocol == 'tcp':  # Only consider TCP for this example
-                    protocols.add((name, port))
+                if name and port and protocol:
+                    protocols.add(name)
 
-    with open('nftables.conf', 'w') as nftables_file:
-        write_nftables_config(nftables_file, protocols)
+        with open('secmark.sh', 'w') as secmark_file, open('type_attr.txt', 'w') as type_attr_file:
+            # Write attributes
+            write_attributes(type_attr_file, protocols)
+            # Separate attributes and types with a newline
+            type_attr_file.write('\n')
+            # Write types
+            write_types(type_attr_file, protocols)
+
+            # Reset file pointer to beginning for secmark commands
+            services_file.seek(0)
+
+            for line in services_file:
+                if line.strip() and not line.startswith('#'):
+                    name, port, protocol = parse_service_line(line)
+                    if name and port and protocol:
+                        write_secmark_commands(secmark_file, name, port, protocol)
 
 process_services_file()
-
