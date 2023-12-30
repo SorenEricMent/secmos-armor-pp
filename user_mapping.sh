58642e040d54497556a17be8cc30100b4c42a7a8
Yunchuan "Winslow" Hu <i@winsloweric.com>
2023-12-30 07:53:53 -0800

    updated secmos seusers

--- user_mapping.sh
+++ user_mapping.sh
@@ -1,3 +1,4 @@
 sudo semanage login -a -s certadm_u -r s0:s0 certadm
 sudo semanage login -a -s sysadm_u -r s0-s15:c0.c1023 sysadm
-sudo semanage login -a -s secadm_u -r s0-s15:c0.c1023 secadm
+sudo semanage login -a -s secadm_u -r s0-s15:c0.c512 secadm
+sudo semanage login -a -s sbadm_u -r s0-s15:c0.c512 sbadm
